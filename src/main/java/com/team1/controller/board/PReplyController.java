9d0bbe31a5ab7cee3a624fd5ebd8dea65c43eb83
jiyeon <devjenlee@gmail.com>
2021-12-28 11:35:48 +0900

    12/28 am11:35
    재요청

--- src/main/java/com/team1/controller/board/PReplyController.java
+++ src/main/java/com/team1/controller/board/PReplyController.java
@@ -1,11 +1,14 @@
 package com.team1.controller.board;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.team1.domain.board.PReplyVO;
 import com.team1.service.board.PReplyService;
 
 import lombok.Setter;
@@ -19,6 +22,11 @@ public class PReplyController {
 	
 	@GetMapping("/list")
 	public void list(Model model) {
+		List<PReplyVO> list = service.getList();
+		
+		
+		
+		
 		
 	}
 	
